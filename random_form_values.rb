84865f9fcd85f4dd8a1d39f6df93088cad4e51fd
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-06-14 19:27:34 +0100

    Initial commit

---
+++ random_form_values.rb
@@ -0,0 +1,27 @@
+require 'faker'
+
+class RandomFormValues
+  include Faker
+
+  def generate_first_name
+    @first_name = Faker::BackToTheFuture.character
+  end
+
+  def generate_last_name
+    @last_name = Faker::DrWho.character
+  end
+
+  def generate_phone_number
+    @phone_num = Faker::PhoneNumber.phone_number
+  end
+
+  def generate_password
+    @password = Faker::Internet.password
+  end
+
+  def generate_email_address
+    @email_address = Faker::Internet.email
+  end
+
+
+end #end of class
